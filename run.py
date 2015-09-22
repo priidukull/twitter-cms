4fa4c5e5890eb51e6f749ffdd91d3f190037e6ae
Priidu Kull <priidu.kull@gmail.com>
2015-09-22 12:57:51 +0300

    So that the app would be running with host 0.0.0.0 and port 80 and be visible to the world

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!flask/bin/python
+import os
 from app import app
-app.run(debug=True)
+
+if __name__ == '__main__':
+    env = os.environ.get('ENVIRONMENT')
+    if env == 'twitter':
+        app.run('0.0.0.0', 80)
+    else:
+        app.run(debug=True)
