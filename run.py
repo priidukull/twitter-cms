5a4bda0b665973fb090852cc015b9ffa76fb37cd
Priidu Kull <priidu.kull@gmail.com>
2015-09-23 10:51:00 +0300

    Using deploy script obtained from https://github.com/mattupstate/ansible-tutorial

--- run.py
+++ run.py
@@ -3,8 +3,4 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    env = os.environ.get('ENVIRONMENT')
-    if env == 'twitter':
-        app.run('0.0.0.0', 80)
-    else:
-        app.run(debug=True)
+    app.run()
